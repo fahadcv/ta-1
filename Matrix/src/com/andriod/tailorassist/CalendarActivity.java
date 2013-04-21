c46c62c125fec6771a7de9d057edc7174178bdd4
shihab <sihabkv@gmail.com>
2013-04-21 08:40:13 +0530

    added calendar activity

---
+++ Matrix/src/com/andriod/tailorassist/CalendarActivity.java
@@ -0,0 +1,22 @@
+package com.andriod.tailorassist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class CalendarActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calendar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.calendar, menu);
+		return true;
+	}
+
+}
