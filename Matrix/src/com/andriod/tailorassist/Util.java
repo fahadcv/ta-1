103e7361f4c3ac759d2a90b00d3a95e5e07d95dd
shihab <sihabkv@gmail.com>
2013-04-22 22:08:48 +0530

    date logic

--- Matrix/src/com/andriod/tailorassist/Util.java
+++ Matrix/src/com/andriod/tailorassist/Util.java
@@ -1,8 +1,13 @@
 package com.andriod.tailorassist;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import android.widget.EditText;
 
 public class Util {
+	static SimpleDateFormat DBdateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	static SimpleDateFormat viewDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 	public static boolean isNumeric(String value){
 		try{
 			Long.parseLong(value);
@@ -52,5 +57,15 @@ public class Util {
 		return !(Util.isEmpty(oldValue) && (editField == null || Util.isEmpty(editField.getText().toString()))
 				|| (editField != null && editField.getText()!=null && editField.getText().toString().trim().equals(oldValue))) ;
 	
+	}
+	public static String convertToDateview(String dbDate){
+		
+		try {
+			return viewDateFormat.format(DBdateFormat.parse(dbDate));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
 	}
 }
